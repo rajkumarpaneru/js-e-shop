0848ed71bf12f8ea023ae70010d4946e879a06fb
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-23 17:52:11 +0545

    move add-to-cart function to cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHTML = '';
@@ -58,26 +58,6 @@ products.forEach((product) => {
 
 document.querySelector('.js-products-grid').
 innerHTML = productsHTML;
-
-function addToCart(productId){
-    
-    let matchingItem;
-
-    cart.forEach((item) => {
-        if(productId === item.productId){
-            matchingItem = item;
-        }
-    });
-
-    if(matchingItem){
-        matchingItem.quantity += 1;
-    } else {
-        cart.push({
-            productId: productId,
-            quantity: 1
-        });
-    }
-}
 
 function updateCartQuantity(){
     let cartQuantity = 0;
