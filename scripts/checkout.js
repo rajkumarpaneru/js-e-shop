45d4bba20c70a76fe5e61af0e0dbb6e49c5c84cb
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-29 15:01:12 +0545

    use Promise to load data

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,18 @@ import { renderOrderSummery } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+        });
+    }).then(() => {
+        renderOrderSummery();
+        renderPaymentSummary();
+    });
+
+/*
 loadProducts(() => {
     renderOrderSummery();
     renderPaymentSummary();
 });
+*/
